76e3be4874a91bba6e198a514dba1123d170a1a4
molikuner <molikuner@gmail.com>
2019-12-04 20:07:53 +0100

    Upload 03 and 04

---
+++ 04_2/index.ts
@@ -0,0 +1,20 @@
+const input = "272091-815432";
+const a = BigInt(input.split("-")[0]);
+const b = BigInt(input.split("-")[1]);
+
+function countCandidates(regex: RegExp): bigint {
+    function ascending(input: string): boolean {
+        for (let i = 0; i < input.length - 1; i++) {
+            if (BigInt(input.charAt(input.length - i - 1)) < BigInt(input.charAt(input.length - i - 2))) return false;
+        }
+        return true;
+    }
+    let count = 0n;
+    for (let i = a; i <= b; i++) {
+        const iString = i.toString();
+        if (regex.test(iString) && ascending(iString)) count++;
+    }
+    return count;
+}
+
+console.log("Part 2:", countCandidates(RegExp("(?:(\\d)(?!\\1)(\\d)\\2(?!\\2))|(?:^(\\d)\\3(?!\\3))")));
